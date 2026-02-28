86daa0975a05fbeeecddd7b29e1b801727d755f0
fityanrahman <fi.elrahman6@gmail.com>
2026-02-28 13:03:54 +0700

    fix: prevent duplicate FloatingActionButton heroTag assertion during navigation
    - Added padding on textformfield in add item screen

---
+++ lib/const/hero_tag_const.dart
@@ -0,0 +1,13 @@
+class HeroTagConst {
+  HeroTagConst._();
+
+  // List Screen
+  static const String addListButton = 'add_list_button';
+  static const String clearListButton = 'clear_list_button';
+
+  // Add Item Screen
+  static const String confirmAddButton = 'confirm_add_button';
+
+  // Add Notes Screen
+  static const String confirmNotesButton = 'confirm_notes_button';
+}
--- lib/screens/add_notes_screen.dart
+++ lib/screens/add_notes_screen.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:router_experiment/const/hero_tag_const.dart';
 
-class AddNotesScreen extends StatefulWidget {
+class AddNotesScreen extends StatelessWidget {
   const AddNotesScreen({super.key});
 
-  @override
-  State<AddNotesScreen> createState() => _AddNotesScreenState();
-}
-
-class _AddNotesScreenState extends State<AddNotesScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,6 +13,7 @@ class _AddNotesScreenState extends State<AddNotesScreen> {
         child: Text('Add Notes Screen'),
       ),
       floatingActionButton: FloatingActionButton(
+        heroTag: HeroTagConst.confirmNotesButton,
         onPressed: () {
           context.pop(true);
         },
